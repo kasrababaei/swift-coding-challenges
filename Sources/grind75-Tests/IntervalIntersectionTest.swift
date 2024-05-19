c9e7cbb810fe2ad1f6fc04b5ea632ad27f52c1ed
Kasra Babaei <kasrababaei@gmail.com>
2024-05-19 14:23:29 -0600

    IntervalIntersection

---
+++ Sources/grind75-Tests/IntervalIntersectionTest.swift
@@ -0,0 +1,44 @@
+import XCTest
+@testable import grind75
+
+final class IntervalIntersectionTest: XCTestCase {
+    func testOne() {
+        let a = [[0,2],[5,10],[13,23],[24,25]]
+        let b = [[1,5],[8,12],[15,24],[25,26]]
+        let expected = [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+        
+        XCTAssertEqual(IntervalIntersection.intervalIntersection(a, b), expected)
+    }
+    
+    func testTwo() {
+        let a = [[1,3],[5,9]]
+        let b: [[Int]] = []
+        let expected: [[Int]] = []
+        
+        XCTAssertEqual(IntervalIntersection.intervalIntersection(a, b), expected)
+    }
+    
+    func testThree() {
+        let a = [[8,15]]
+        let b = [[2,6],[8,10],[12,20]]
+        let expected = [[8,10],[12,15]]
+        
+        XCTAssertEqual(IntervalIntersection.intervalIntersection(a, b), expected)
+    }
+    
+    func testFour() {
+        let a = [[3,5],[9,20]]
+        let b = [[4,5],[7,10],[11,12],[14,15],[16,20]]
+        let expected = [[4,5],[9,10],[11,12],[14,15],[16,20]]
+        
+        XCTAssertEqual(IntervalIntersection.intervalIntersection(a, b), expected)
+    }
+    
+    func testFive() {
+        let a = [[4,6],[7,8],[10,17]]
+        let b = [[5,10]]
+        let expected = [[5,6],[7,8],[10,10]]
+        
+        XCTAssertEqual(IntervalIntersection.intervalIntersection(a, b), expected)
+    }
+}
---
+++ Sources/grind75/Problems/IntervalIntersection.swift
@@ -0,0 +1,59 @@
+/*
+You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+A closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
+
+The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+
+Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Example 2:
+
+Input: firstList = [[1,3],[5,9]], secondList = []
+Output: []
+ 
+ Constraints:
+
+ 0 <= firstList.length, secondList.length <= 1000
+ firstList.length + secondList.length >= 1
+ 0 <= starti < endi <= 109
+ endi < starti+1
+ 0 <= startj < endj <= 109
+ endj < startj+1
+ 
+*/
+
+enum IntervalIntersection {
+    static func intervalIntersection(_ firstList: [[Int]], _ secondList: [[Int]]) -> [[Int]] {
+        var intersections: [[Int]] = []
+        var leadIndex = 0
+        var trailIndex = 0
+        
+        while leadIndex < firstList.endIndex, trailIndex < secondList.endIndex {
+            let first = firstList[leadIndex]
+            let second = secondList[trailIndex]
+            
+            let lowerBound = max(first[0], second[0])
+            let upperBound = min(first[1], second[1])
+            
+            if upperBound >= lowerBound {
+                intersections.append([lowerBound, upperBound])
+                if first[1] < second[1] {
+                    leadIndex += 1
+                } else {
+                    trailIndex += 1
+                }
+            } else {
+                if first[0] > second[1] {
+                    trailIndex += 1
+                } else {
+                    leadIndex += 1
+                }
+            }
+        }
+        
+        return intersections
+    }
+}
