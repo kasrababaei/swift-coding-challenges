8a0b8886397734c6028081d8ec3727569ca90773
Kasra Babaei <kasrababaei@gmail.com>
2024-09-23 20:59:17 -0600

    LinkedListCycle

---
+++ Sources/Challenges-Tests/LinkedListCycleTests.swift
@@ -0,0 +1,28 @@
+import Testing
+@testable import Challenges
+
+struct Test {
+    @Test func hasCycleOne() async throws {
+        // head = [3, 2, 0, -4], pos = 1
+        let four = ListNode(-4)
+        let zero = ListNode(0, four)
+        let two = ListNode(2, zero)
+        four.next = two
+        let one = ListNode(3, two)
+        #expect(LinkedListCycle.hasCycle(one))
+    }
+    
+    @Test func hasCycleTwo() async throws {
+        // head = [1, 2], pos = 0
+        let two = ListNode(2)
+        let one = ListNode(1, two)
+        two.next = one
+        #expect(LinkedListCycle.hasCycle(one))
+    }
+    
+    @Test func hasCycleThree() async throws {
+        // head = [1], pos = -1
+        let one = ListNode(1)
+        #expect(LinkedListCycle.hasCycle(one) == false)
+    }
+}
