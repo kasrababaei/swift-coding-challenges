b84cd632f2f197c89b2d4f2b4be06102ab03b7b4
Kasra Babaei <kasrababaei@gmail.com>
2024-09-25 22:36:36 -0600

    MiddleOfLinkedList

---
+++ Sources/Challenges-Tests/MiddleOfLinkedListTests.swift
@@ -0,0 +1,25 @@
+import Testing
+@testable import Challenges
+
+struct MiddleOfLinkedListTests {
+    @Test func odd() async throws {
+        let five = ListNode(5)
+        let four = ListNode(4, five)
+        let three = ListNode(3, four)
+        let two = ListNode(2, three)
+        let one = ListNode(1, two)
+        
+        #expect(MiddleOfLinkedList.middleNode(one) === three)
+    }
+    
+    @Test func even() async throws {
+        let six = ListNode(6)
+        let five = ListNode(5, six)
+        let four = ListNode(4, five)
+        let three = ListNode(3, four)
+        let two = ListNode(2, three)
+        let one = ListNode(1, two)
+        
+        #expect(MiddleOfLinkedList.middleNode(one) === four)
+    }
+}
